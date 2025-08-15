57ce489f69ae3eda36c65267f4cd55cba22642bf
Nick Walker <nick@nickwalker.us>
2025-08-15 16:16:33 -0700

    Drop CSP policy
    Have to leave very loose due to 3rd party js and maplibre anyways
    Plug potential gallery XSS holes

--- app/security.py
+++ app/security.py
@@ -14,23 +14,7 @@ limiter = Limiter(key_func=get_remote_address)
 def add_security_headers(response):
     """Add security headers to all responses"""
 
-    # Content Security Policy - restrictive but allows necessary resources
-    csp_policy = (
-        "default-src 'self'; "
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://cdnjs.cloudflare.com; "
-        "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com https://cdnjs.cloudflare.com; "
-        "font-src 'self' https://fonts.gstatic.com; "
-        "img-src 'self' data: blob: https://www.gravatar.com https://graph.microsoft.com https://avatars.githubusercontent.com https://lh3.googleusercontent.com; "
-        "connect-src 'self' https://cdn.jsdelivr.net https://tiles.openfreemap.org; "
-        "worker-src blob:; "
-        "frame-ancestors 'none'; "
-        "base-uri 'self'; "
-        "form-action 'self' https://accounts.google.com https://github.com https://login.microsoftonline.com; "
-        "object-src 'none';"
-    )
-
     # Security headers
-    response.headers['Content-Security-Policy'] = csp_policy
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'DENY'
     response.headers['X-XSS-Protection'] = '1; mode=block'
