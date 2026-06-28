a4fae2c6c4a9f5583cd98448b75bd1169700a8bc
Nick Walker <nick@nickwalker.us>
2026-06-28 15:25:43 -0500

    Expose public stats and results JSON endpoints
    Ignore out-of-event window uploads
    Set CORS headers for cross site access

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,11 +1,11 @@
 import datetime
 
-from flask import Blueprint, render_template, current_app, send_from_directory, jsonify, make_response
+from flask import Blueprint, render_template, current_app, send_from_directory, jsonify, make_response, app
 from markupsafe import Markup
 import os
 
 from app import Config
-from app.models import TeamMembershipStatus
+from app.models import TeamMembershipStatus, TeamFormat, TeamStatus
 
 main = Blueprint('main', __name__)
 
@@ -35,15 +35,21 @@ def terms_of_service():
                          contact_email=current_app.config.get('CONTACT_EMAIL'))
 
 
-@main.route('/stats')
+@main.route('/stats.json')
 def global_stats():
     """Returns global event statistics (unauthenticated endpoint)"""
     from app.models import Team, TeamMembership, TeamMembershipStatus, User
 
-    # Count total teams that are open full or closed
-    team_count = Team.query.filter(Team.status.in_([
-        'OPEN', 'FULL', 'CLOSED'
-    ])).count()
+    # Count total teams that are open or closed (not pending or cancelled)
+    team_count = Team.query.filter(
+        Team.status.in_([TeamStatus.OPEN, TeamStatus.CLOSED]),
+        Team.format == TeamFormat.TEAM
+    ).count()
+
+    solo_count = Team.query.filter(
+        Team.format==TeamFormat.SOLO,
+        Team.status.in_([TeamStatus.OPEN, TeamStatus.CLOSED])
+    ).count()
 
     # Count total active team memberships
     membership_count = TeamMembership.query.filter_by(
@@ -55,6 +61,7 @@ def global_stats():
 
     response = make_response(jsonify({
         'teams': team_count,
+        'solos': solo_count,
         'memberships': membership_count,
         'users': user_count
     }))
@@ -66,7 +73,7 @@ def global_stats():
 
     return response
 
-@main.route('/results')
+@main.route('/results.json')
 def results():
     """
     API endpoint to get times for each exchange based on uploaded images.
@@ -74,16 +81,21 @@ def results():
     """
     # For each team, get the images, grouped by associated exchange
     from app.models import Team, Image
-    from collections import defaultdict
-    teams = Team.query.all()
+    teams = Team.query.filter(
+        Team.status.in_([TeamStatus.OPEN, TeamStatus.CLOSED])).order_by(Team.name).all()
     results = []
+    latest_upload_time = None
+
+    minimum_time = Config.EVENT_START_TIME.astimezone(datetime.UTC).replace(tzinfo=None)
+    maximum_time = minimum_time + datetime.timedelta(hours=16)
+
     for team in teams:
         format = team.format.value
         team_size = len([m for m in team.memberships if m.status == TeamMembershipStatus.ACTIVE])
         if team_size == 6:
             format = 'Competitive'
         team_data = {
-            'teamName': team.name,
+            'name': team.name,
             "category": format,
             "teamSize": team_size,
             'exchangeTimes': {}
@@ -94,15 +106,45 @@ def results():
                 img_exchange_id = img.associated_exchange_id if img.associated_exchange_id else img.manual_exchange_id
                 if img_exchange_id in team_data['exchangeTimes']:
                     # Check if this image is later than the last one for this exchange. We take the latest image
-                    last_img = team_data['exchanges'][img_exchange_id]
-                    if img.capture_time <= last_img['capture_time']:
+                    last_img_capture_time = team_data['exchangeTimes'][img_exchange_id]
+                    if img.capture_time <= last_img_capture_time:
                         continue
-                team_data['exchangeTimes'][img_exchange_id] = img.capture_time
+                if current_app.jinja_env.globals['is_production'] and (img.capture_time > maximum_time):
+                    # In production, ignore images with upload times outside the event window
+                    continue
+                if img.capture_time < minimum_time and img_exchange_id == "165":
+                    team_data['exchangeTimes'][img_exchange_id] = minimum_time
+                elif img.capture_time < minimum_time:
+                    # Ignore images with capture times before the event start time, except for the first exchange which we set to start time
+                    continue
+                else:
+                    team_data['exchangeTimes'][img_exchange_id] = img.capture_time
+
+                # Track the latest upload time across all images used in results
+                if latest_upload_time is None or img.upload_time > latest_upload_time:
+                    latest_upload_time = img.upload_time
+
         for exchange_id in team_data['exchangeTimes']:
             team_data['exchangeTimes'][exchange_id] -= Config.EVENT_START_TIME.astimezone(datetime.UTC).replace(tzinfo=None)
             team_data['exchangeTimes'][exchange_id] = int(team_data['exchangeTimes'][exchange_id].total_seconds())
         results.append(team_data)
-    return jsonify(results)
+
+    response = make_response(jsonify({
+        'starts' : {
+            'main': {
+                'time': Config.EVENT_START_TIME.isoformat(),
+            },
+        },
+        'results': results,
+        'lastUpdated': latest_upload_time.astimezone(datetime.UTC).replace(tzinfo=None).isoformat() if latest_upload_time else None,
+    }))
+
+    # Add CORS headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+    return response
 
 @main.route('/.well-known/microsoft-identity-association.json')
 def microsoft_identity_association():
